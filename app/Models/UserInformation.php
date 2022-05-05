61539df1cdd3883c38b9cce1c9a61caffc904603
Boris Obrembski <boris@sportiw.com>
2022-05-05 15:57:20 +0200

    init

---
+++ app/Models/UserInformation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserInformation extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+}
